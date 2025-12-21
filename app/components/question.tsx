2c4f1922cb75aeef24cf38df64c7602732af6128
sd-codebase <krishna.dhas021815@gmail.com>
2025-12-21 23:05:35 +0530

    Update question.tsx

--- app/components/question.tsx
+++ app/components/question.tsx
@@ -32,7 +32,7 @@ export const Question = ({ question, showPyo, resourceUrl }: QuestionProps) => {
                 style={{ marginBottom: "0.5rem" }}
                 gap={"0.25rem"}
               >
-                <Text strong style={{ fontSize: "1rem" }}>
+                <Text strong style={{ fontSize: "1rem", minWidth: "2rem" }}>
                   {opKey})
                 </Text>
 
